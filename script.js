2dfa70798977173a2d3da74113194649b699cab6
jassmin-herr <jassminfernandez@gmail.com>
2021-10-06 22:31:18 -0500

    adds flip card logic

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // Audio 
 
 class AudioController {
@@ -41,16 +40,59 @@ class AudioController {
 
  }
 }
+class MixorMatch {
+    constructor(totalTime, cards){
+      this.cardsArray = cards;
+      this.totalTime = totalTime;
+      this.timeRemaining = totalTime;
+      this.timer = document.getElementById('time-remaining');
+      this.ticker = document.getElementbyId('flips');
+      this.audioController = new AudioController();
+      
+    }
+  startGame() {
+      this.cardToCheck = null;
+      this.totalClicks = 0;
+      this.timeRemaining = this.totalTime;
+      this.matchedCards = []; // matched cards into this array. 
+      this.busy = true; // animation is happening right now. 
+
+  }
+  flipCard(card) {
+      //check whether or not can flip card
+      if(this.canFlipCard(card)) {
+          this.audioController.flip();
+          this.totalClicks++;
+          this.ticker.innerText = this.totalClicks;
+          card.cardList.add('visible');
+
+
+      }
+
+  }
+   
+  //if returns true then card can be flipped. all 3 statements have to be false for it to be True. 
+  canFlipCard(card) {
+  return true;
+  //return(!this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck) 
+
+  }
+
+}
+
+
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text')); 
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixorMatch(100, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
+              game.startGame();
 
-            //game. start Game
+            
             let audioController = new AudioController ();
           audioController.startMusic();
 
@@ -60,7 +102,7 @@ function ready() {
     });
   cards.forEach(card => {
      card.addEventListener('click', () => {
-        // game flip card function
+         game.flipCard(card)
 
      });
   });
